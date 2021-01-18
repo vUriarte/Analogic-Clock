14640685d71dcc5e5b4b07e62d59a9bf246fee48
uriarte <uriarte0505@gmail.com>
2021-01-17 22:24:18 -0300

    Bug Fix: Clock used to return to 0 counter clockwise when seconds pointer hit 0

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
+var date = new Date()
+//console.log(data)
+
+let hour = date.getHours()
+let minutes = date.getMinutes()
+let seconds =  date.getSeconds()
+
+
+let posicaoHr = (hour * 360) / 12 + ((minutes * 360) / 60) / 12
+let posicaoMin = (minutes * 360) / 60 + ((seconds * 360) / 60) / 60
+let posicaoSeg = (seconds * 360) / 60
+
 
 function executeClock(){
-    var date = new Date()
-    //console.log(data)
 
-    let hour = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds =  date.getSeconds()
-
-
-    let posicaoHr = (hour * 360) / 12 + ((minutes * 360) / 60) / 12
-    let posicaoMin = (minutes * 360) / 60 + ((seconds * 360) / 60) / 60
-    let posicaoSeg = (seconds * 360) / 60
-
-
+    posicaoHr = posicaoHr + (3 / 360)
+    posicaoMin = posicaoMin + (6 / 60)
+    posicaoSeg = posicaoSeg + 6;
 
     PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)"
     PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)"
