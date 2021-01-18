76de39837d823b014ac4584aa372fe98eb2af2eb
uriarte <uriarte0505@gmail.com>
2021-01-17 21:18:19 -0300

    The clock now moves with a seg interval

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
-var date = new Date()
-//console.log(data)
 
-let hour = date.getHours()
-let minutes = date.getMinutes()
-let seconds =  date.getSeconds()
+function executeClock(){
+    var date = new Date()
+    //console.log(data)
 
-
-let posicaoHr = (hour * 360) / 12 + ((minutes * 360) / 60) / 12
-let posicaoMin = (minutes * 360) / 60 + ((seconds * 360) / 60) / 60
-let posicaoSeg = (seconds * 360) / 60
+    let hour = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds =  date.getSeconds()
 
 
+    let posicaoHr = (hour * 360) / 12 + ((minutes * 360) / 60) / 12
+    let posicaoMin = (minutes * 360) / 60 + ((seconds * 360) / 60) / 60
+    let posicaoSeg = (seconds * 360) / 60
 
-PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)"
-PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)"
-PONTEIROSEGUNDO.style.transform =  "rotate(" + posicaoSeg + "deg)"
+
+
+    PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)"
+    PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)"
+    PONTEIROSEGUNDO.style.transform =  "rotate(" + posicaoSeg + "deg)"
+
+}
+
+var interval = setInterval(executeClock, 1000)
